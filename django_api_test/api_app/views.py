b92b3b8599ef227723af3ea7acc1fca10b77d10a
alboyz <alboyzskeaters@gmail.com>
2021-04-18 06:50:38 +0700

    class based API VIEW

--- django_api_test/api_app/views.py
+++ django_api_test/api_app/views.py
@@ -1,12 +1,48 @@
-from .models import Article
-from .serializers import ArticleSerializer
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import APIView
+from .models import Article
+from .serializers import ArticleSerializer
 # Create your views here.
 
+class ArticleList(APIView):  #pylint: disable=missing-class-docstring    
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data) 
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetails(APIView):
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data) 
+
+    def put(self, request, id): 
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+'''
 @api_view(['GET', 'POST'])
 def article_list(request):
     # Get All Article
@@ -43,5 +79,7 @@ def article_details (request, pk):
     elif request.method == 'DELETE':
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    '''
 
 
